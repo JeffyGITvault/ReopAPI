7fc831926d8349c329a2e19e03767dd98fd9a7d4
JeffyGITvault <jeffrey.guenthner@gmail.com>
2025-05-02 15:55:34 -0700

    Update main.py Groq Trace

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
-# app/main.py
-
 from fastapi import FastAPI
 from app.api.SECAPI import app as secapi_app
 from app.api.run_pipeline import router as pipeline_router
+from app.api.tools.groq_ping import router as groq_ping_router
 
 app = FastAPI(
     title="Your New Multi-Agent API",
@@ -14,3 +13,6 @@ app.mount("/secapi", secapi_app)
 
 # Include new /run_pipeline route
 app.include_router(pipeline_router)
+
+# Include Groq test route
+app.include_router(groq_ping_router)
